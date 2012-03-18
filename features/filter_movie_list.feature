1e1546e4c7ad766dcb4f0b6d2ba698401b746a11
kristinh <kristinh@precis-project.com>
2012-03-18 21:05:15 +0100

    filter scenarios working, part2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,24 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R 
+  And I uncheck the following ratings: PG-13, G, NC-17 
+  And I press "ratings_submit"
+  Then I should not see "Aladdin"
+  And I should not see "Chicken RuChicken Run"
+  And I should see "The Incredibles"
+  And I should see "The Terminator"
   # enter step(s) to uncheck all other checkboxes
   # enter step to "submit" the search form on the homepage
   # enter step(s) to ensure that PG and R movies are visible
   # enter step(s) to ensure that other movies are not visible
 
-Scenario: no ratings selected
-  # see assignment
-
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: PG, R, PG-13, NC-17, G 
+  And I press "ratings_submit"
+  Then I should see all of the movies
+
+Scenario: no ratings selected
+  When I uncheck the following ratings: PG, R, PG-13, NC-17, G 
+  And I press "ratings_submit"
+  Then I should see none of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -17,12 +17,37 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   assert false, "Unimplmemented"
 end
 
+When /^(?:|I )check the "([^"]*)" rating$/ do |field|
+  check('ratings_' + field)
+end
+
+When /^(?:|I )uncheck the "([^"]*)" rating$/ do |field|
+  uncheck('ratings_' + field)
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+When /I (un)?check the following ratings: (.*)/ do |uncheck, ratings|
+  ratings.split(',').each do |rating|
+    if uncheck
+      When  %Q{I uncheck the "#{rating.strip}" rating}
+    else
+      When  %Q{I check the "#{rating.strip}" rating}
+    end
+  end
 end
+
+Then /I should see all of the movies/ do 
+  #puts page.body
+  #save_and_open_page
+  rows = page.all(:xpath, "//table[@id='movies']/tbody/tr").size
+  assert_equal Movie.count, rows
+end
+
+Then /I should see none of the movies/ do 
+  rows = page.all(:xpath, "//table[@id='movies']/tbody/tr").size
+  assert_equal 0, rows
+end
+
